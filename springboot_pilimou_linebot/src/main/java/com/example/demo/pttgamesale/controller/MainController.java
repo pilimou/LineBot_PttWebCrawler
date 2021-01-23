8e54239a67a30c959c6a8bf5b5f880d9d8d2cfe5
pilimou <soulms55@gmail.com>
2021-01-24 00:07:31 +0800

    控制排程撈資料

--- springboot_pilimou_linebot/src/main/java/com/example/demo/pttgamesale/controller/MainController.java
+++ springboot_pilimou_linebot/src/main/java/com/example/demo/pttgamesale/controller/MainController.java
@@ -1,15 +1,45 @@
 package com.example.demo.pttgamesale.controller;
 
-import com.example.demo.pttgamesale.test.HttpClientTest;
-import com.example.demo.unit.SaveFile;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.example.demo.pttgamesale.http.HttpClientTest;
+import com.example.demo.util.LoadFile;
+import com.example.demo.util.SaveFile;
 
 public class MainController {
 
-	public static void main(String[] args) {
-		String url = "https://www.ptt.cc/bbs/Gamesale/index.html";
-		SaveFile aaa = new SaveFile();
-		HttpClientTest httpClientTest = new HttpClientTest();
-		httpClientTest.getHtml(url);
+	@Value("${pttGameSale.Url}")
+	private String url;
+	
+	@Scheduled(cron = "0 */4 8-23 * * * ?")
+	public void getAddNewTitles() {
+		Map<String, String> newTitles = new HttpClientTest().getHtml(url);
+		Map<String, String> oldTitles = new LoadFile().loadPttGameSaleTitle();
+		Map<String, String> addNewTitles = new LinkedHashMap<>();
+		
+		for(Entry<String, String> newEntry : newTitles.entrySet()) {
+			boolean flag = true;
+			for(Entry<String, String> oldEntry : oldTitles.entrySet()) {
+				if(newEntry.getValue().equals(oldEntry.getValue())) {
+					flag = false;
+				}
+			}
+			if(flag) {
+				addNewTitles.put(newEntry.getKey(),newEntry.getValue());
+			}
+		}	
+		
+		if(addNewTitles.size() > 0) {
+			new SaveFile().savePttGameSaleTitle(newTitles);
+			System.out.println("新增" + addNewTitles.size() + "筆");
+		}else {
+			System.out.println("新增" + addNewTitles.size() + "筆");
+		}
 
 	}
 
